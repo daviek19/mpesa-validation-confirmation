6c4aeb9f22fda5ab03e2b9dcf6e4cdaf71f6fdc7
davie <daviek19@gmail.com>
2018-08-31 13:46:31 +0300

    added stk-push callback

---
+++ src/main/kotlin/com/sracs/sracsc2blistner/mpesaStkPush/StkPushController.kt
@@ -0,0 +1,29 @@
+package com.sracs.sracsc2blistner.mpesaStkPush;
+
+import com.sracs.sracsc2blistner.mpesaC2b.MpesaC2bController
+import org.omg.CORBA.Object
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = arrayOf("v1/stk-push"),
+        headers = arrayOf("Accept=application/json"),
+        produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+public class StkPushController {
+
+    private val LOGGER = LoggerFactory.getLogger(StkPushController::class.java)
+
+    @PostMapping("/call-back")
+    fun c2bConfirmation(@RequestBody postData: Any): ResponseEntity<*> {
+
+        LOGGER.info("stk push results {}", postData.toString());
+
+        return ResponseEntity<Any>(null, HttpStatus.ACCEPTED);
+    }
+}
